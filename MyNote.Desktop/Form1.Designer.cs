f8cc0fa4296b3df36cbb84fa1516edabd27a7b48
hakan ölçer <ha_-kan-_olcer95@windowslive.com>
2020-06-01 21:42:19 +0300

    Windows form api call

---
+++ MyNote.Desktop/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace MyNote.Desktop
+{
+    partial class form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                client.Dispose();
+            }
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sadasd = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btnListNotes = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtApiUrl = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstNotes = new System.Windows.Forms.ListBox();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sadasd
+            // 
+            this.sadasd.AutoSize = true;
+            this.sadasd.BackColor = System.Drawing.Color.LightCyan;
+            this.sadasd.Location = new System.Drawing.Point(23, 73);
+            this.sadasd.Name = "sadasd";
+            this.sadasd.Size = new System.Drawing.Size(97, 20);
+            this.sadasd.TabIndex = 0;
+            this.sadasd.Text = "User Name :";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BackColor = System.Drawing.Color.Lavender;
+            this.txtUserName.Location = new System.Drawing.Point(27, 105);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(194, 26);
+            this.txtUserName.TabIndex = 1;
+            this.txtUserName.Text = "hakanolcer7@gmail.com";
+            // 
+            // btnListNotes
+            // 
+            this.btnListNotes.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnListNotes.Location = new System.Drawing.Point(550, 103);
+            this.btnListNotes.Name = "btnListNotes";
+            this.btnListNotes.Size = new System.Drawing.Size(148, 31);
+            this.btnListNotes.TabIndex = 2;
+            this.btnListNotes.Text = "List Notes";
+            this.btnListNotes.UseVisualStyleBackColor = false;
+            this.btnListNotes.Click += new System.EventHandler(this.btnListNotes_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.Lavender;
+            this.txtPassword.Location = new System.Drawing.Point(306, 105);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(203, 26);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.Text = "Password1.";
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightCyan;
+            this.label1.Location = new System.Drawing.Point(302, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password:";
+            // 
+            // txtApiUrl
+            // 
+            this.txtApiUrl.BackColor = System.Drawing.Color.Lavender;
+            this.txtApiUrl.Location = new System.Drawing.Point(123, 20);
+            this.txtApiUrl.Name = "txtApiUrl";
+            this.txtApiUrl.ReadOnly = true;
+            this.txtApiUrl.Size = new System.Drawing.Size(575, 26);
+            this.txtApiUrl.TabIndex = 6;
+            this.txtApiUrl.Text = "https://mynoteapi.hakanolcer.xyz/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightCyan;
+            this.label2.Location = new System.Drawing.Point(23, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "API URL :";
+            // 
+            // lstNotes
+            // 
+            this.lstNotes.BackColor = System.Drawing.Color.Lavender;
+            this.lstNotes.FormattingEnabled = true;
+            this.lstNotes.ItemHeight = 20;
+            this.lstNotes.Location = new System.Drawing.Point(27, 177);
+            this.lstNotes.Name = "lstNotes";
+            this.lstNotes.Size = new System.Drawing.Size(233, 304);
+            this.lstNotes.TabIndex = 7;
+            this.lstNotes.SelectedIndexChanged += new System.EventHandler(this.lstNotes_SelectedIndexChanged);
+            // 
+            // txtNote
+            // 
+            this.txtNote.BackColor = System.Drawing.Color.Lavender;
+            this.txtNote.Location = new System.Drawing.Point(306, 177);
+            this.txtNote.Multiline = true;
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(392, 304);
+            this.txtNote.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LightCyan;
+            this.label3.Location = new System.Drawing.Point(23, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Titles :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LightCyan;
+            this.label4.Location = new System.Drawing.Point(302, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Content :";
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(737, 517);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.lstNotes);
+            this.Controls.Add(this.txtApiUrl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnListNotes);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.sadasd);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "form";
+            this.Text = "MyNote Desktop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label sadasd;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btnListNotes;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtApiUrl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstNotes;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
