0899ead020e6d7f6b8211107524ccb9655d05a0e
hakan ölçer <ha_-kan-_olcer95@windowslive.com>
2020-06-03 22:16:14 +0300

    error , success, register submit ,  resetLoginForm , resetRegisterForm , loading

--- MyNote.UI/Scripts/index.js
+++ MyNote.UI/Scripts/index.js
@@ -1,15 +1,79 @@
 ﻿//AngularJs Version
-
+var apiUrl = "https://mynoteapi.hakanolcer.xyz/"
 var app = angular.module("myApp", ["ngRoute"]);
 
-app.config(function ($routeProvider) {
 
-    $routeProvider
-        .when("/", { templateUrl: "pages/app.html" })
-        .when("/login", { templateUrl: "pages/login.html" });
+app.directive("messages", function () {
+    return {
+        templateUrl: "/directives/messages.html"
+    };
+
 });
 
-app.controller("myCtrl", function ($scope) {
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", { templateUrl: "pages/app.html", controller: "appController" })
+        .when("/login", { templateUrl: "pages/login.html", controller: "loginController" });
+});
+
+app.controller("mainController", function ($scope, $http) {
+    $scope.isLoading = false;
+    $scope.showLoading = function () {
+        $scope.isLoading = true;
+    };
+    $scope.hideLoading = function () {
+        $scope.isLoading = false;
+    };
+
+    $scope.loginData = function () {
+        var loginDataJson = localStorage["login"] | sessionStorage["login"];
+
+        if (!loginDataJson) {
+            return null;
+        }
+
+        try {
+            return JSON.parse(loginDataJson);
+        } catch (e) {
+            return null;
+        }
+    };
+
+    $scope.token = function () {
+        var loginData = $scope.loginData();
+        if (!loginData) {
+            return null;
+        }
+
+        return loginData.access_token;
+    }
+
+
+    $scope.ajax = function (apiUri, method, data, isAuth, successFunc, errorFunc) {
+        $scope.showLoading();
+        var headers = null;
+
+        if (isAuth)
+            headers = { Authorization: "Bearer " + $scope.token() };
+
+
+        $http({
+            url: apiUrl + apiUri,
+            method: method,
+            headers: headers,
+            data: data
+        }).then(
+            function (response) {
+                successFunc(response);
+                $scope.hideLoading();
+            },
+            function (response) {
+                errorFunc(response);
+                $scope.hideLoading();
+            }
+        );
+    };
+
 
     $scope.checkAuth = function () {
         var tokenJson = localStorage["token"] | sessionStorage["token"];
@@ -26,9 +90,99 @@ app.controller("myCtrl", function ($scope) {
     };
 
     $scope.checkAuth();
-});
 
 
+});   //html den sorumlu ana controller
+
+app.controller("loginController", function ($scope, $http) {
+
+    $scope.currentTab = "login";  //  login |  register
+    $scope.messageFor = "login";  // login | register
+    $scope.messageType = "info";  // success | warning |danger | info
+    $scope.messages = [];  // string arrray ["message 1", "message 2"]
+
+    $scope.registerForm = {
+        //web api nin beklediği şekilde. account cont. register kısmındaki
+        Email: "",
+        Password: "",
+        ConfirmPassword: "",
+    };
+
+    $scope.loginForm = {
+        grant_type: "password",
+        username: "",
+        password: ""
+    };
+
+    $scope.rememberMe = false;
+
+    $scope.error = function (data) {
+        $scope.messageFor = $scope.currentTab;
+        $scope.messageType = "danger";
+        $scope.messages = [];
+        if (data.ModelState) {
+            for (var prop in data.ModelState) {
+                //data.ModelState[prop][0];     //gelen nesnenin içinde ModelState diye bir nesne var , onun içinde prop lar var
+                for (var index in data.ModelState[prop]) {
+                    $scope.messages.push(data.ModelState[prop][index]);
+                }
+            }
+        }
+
+
+    };
+
+    $scope.success = function (message) {
+        $scope.messageFor = $scope.currentTab;
+        $scope.messageType = "success";
+        $scope.messages = [message];
+
+    };
+
+    $scope.resetRegisterForm = function () {
+        $scope.registerForm.Email = "";
+        $scope.registerForm.Password = "";
+        $scope.registerForm.ConfirmPassword = "";
+    };
+
+
+    $scope.resetLoginForm = function () {
+        $scope.loginForm.username = "";
+        $scope.loginForm.password = "";
+        $scope.rememberMe = false;
+    };
+
+    $scope.$watch("currentTab", function () {
+        $scope.resetLoginForm();
+        $scope.resetRegisterForm();
+        $scope.messages = [];
+    });
+
+
+    $scope.registerSubmit = function () {
+
+        $scope.ajax("api/Account/Register", "post", $scope.registerForm, false,
+            function (response) {
+                $scope.resetRegisterForm();
+                $scope.success("Your account has been successfully created.")
+            },
+            function (response) {
+                $scope.error(response.data);
+            }
+        );
+
+    };
+
+    $scope.loginSubmit = function () {
+        alert("login submit");
+    };
+});     // view /login
+
+
+app.controller("appController", function ($scope, $location) {
+    $location.path("/login"); //sonra sil
+
+});     //view  /app
 
 // JQuery Document Ready
 $(function () {
@@ -43,4 +197,17 @@ $(function () {
         e.preventDefault()
         $(this).tab('show')
     });
+
+
+    // https://stackoverflow.com/questions/37769900/how-to-change-a-scope-variable-outside-the-controller-in-angularjs
+    // https://www.hiren.dev/2014/06/how-to-access-scope-variable-outside.html
+    $('body').on('shown.bs.tab', 'a[data-toggle="pill"]', function (e) {
+        //var $scope = angular.element($('[ng-controller="mainController"]')[0]).scope();
+        var $scope = angular.element($('[ng-view]')[0]).scope();
+        $scope.currentTab = $(e.target).attr("id") == "pills-signup-tab" ? "register" : "login";
+        $scope.$apply();   // angular a haber veriyoruz. bak bir şeyler değişti seninde arayüz öğelerinde bişeyler değiştirmen gerekebilir.
+        //console.log(e.target); //aktif olan pill i taşıyor
+        //e.target // newly activated tab
+        //e.relatedTarget // previous active tab
+    })
 });
